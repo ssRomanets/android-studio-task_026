4dcb3df43fafe9dc51d66b42e9c121050618fabe
Aleksandr Romanets <a.romanets@yandex.ru>
2024-11-21 09:32:43 +0300

    first commit

---
+++ app/src/main/java/com/example/task_026/ClothesViewModal.kt
@@ -0,0 +1,7 @@
+package com.example.task_026
+
+class ClothesViewModal (
+    val image: Int,
+    val name: String,
+    val description: String,
+)
---
+++ app/src/main/java/com/example/task_026/CustomAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.task_026
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class CustomAdapter(private val clothes: MutableList<ClothesViewModal>):
+    RecyclerView.Adapter<CustomAdapter.ClothesViewHolder>(){
+
+    class ClothesViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val imageViewIV: ImageView = itemView.findViewById(R.id.imageViewIV)
+        val nameTV: TextView = itemView.findViewById(R.id.nameTV)
+        val descriptionTV: TextView = itemView.findViewById(R.id.descriptionTV)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ClothesViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+        return ClothesViewHolder(itemView)
+    }
+
+    override fun getItemCount() = clothes.size
+
+    override fun onBindViewHolder(holder: ClothesViewHolder, position: Int) {
+        val clothesType = clothes[position]
+        holder.imageViewIV.setImageResource(clothesType.image)
+        holder.nameTV.text = clothesType.name
+        holder.descriptionTV.text = clothesType.description
+    }
+}
